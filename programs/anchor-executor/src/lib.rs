1d53978f01f4ffe4776d7928c576c53ba0e9bc8c
Dmitry Redkin <tanatonaut@gmail.com>
2024-05-22 12:58:03 +0300

    could send instruction to system program

--- programs/anchor-executor/src/lib.rs
+++ programs/anchor-executor/src/lib.rs
@@ -1,4 +1,6 @@
-use anchor_lang::prelude::*;
+use anchor_lang::prelude::{*};
+use solana_program::instruction::{Instruction, AccountMeta};
+use solana_program::program::invoke;
 
 declare_id!("FN3asVygEpi3NXFJVrn2SSdvB3PQkKfRNMSR3HJGpofd");
 
@@ -6,7 +8,28 @@ declare_id!("FN3asVygEpi3NXFJVrn2SSdvB3PQkKfRNMSR3HJGpofd");
 pub mod anchor_executor {
     use super::*;
 
-    pub fn execute(ctx: Context<Execute>) -> Result<()> {
+    pub fn execute(ctx: Context<Execute>, ix: ProgramInstruction) -> Result<()> {
+        let account_infos = ctx.remaining_accounts;
+        let mut cpi_accounts = Vec::new();
+        for i in ix.keys {
+            cpi_accounts.push(
+                AccountMeta{
+                    pubkey: i.pubkey,
+                    is_signer: i.is_signer,
+                    is_writable: i.is_writable
+                }
+            );
+        }
+        let instruction = Instruction { 
+            program_id: ix.program_id.key(), 
+            accounts: cpi_accounts, 
+            data: ix.data
+        };
+
+        msg!("Test");
+
+        invoke(&instruction, account_infos)?;
+
         Ok(())
     }
 }
@@ -14,5 +37,22 @@ pub mod anchor_executor {
 #[derive(Accounts)]
 pub struct Execute<'info> {
     #[account(mut)]
-    signer: Signer<'info>
+    signer: Signer<'info>,
+    system_program: Program<'info, System>
+}
+
+// #[derive(BorshSerialize, BorshDeserialize)]
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct DataAccountMeta {
+    pub pubkey: Pubkey,
+    pub is_signer: bool,
+    pub is_writable: bool,
+}
+
+// #[derive(BorshSerialize, BorshDeserialize)]
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ProgramInstruction {
+    pub program_id: Pubkey,
+    pub keys: Vec<DataAccountMeta>,
+    pub data: Vec<u8>,
 }
--- tests/anchor-executor.ts
+++ tests/anchor-executor.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorExecutor } from "../target/types/anchor_executor";
-import {createToken, createTokenAccount, mintTo} from "./helpers/token";
+import { expect } from "chai";
 
 const provider = anchor.AnchorProvider.env();
 
@@ -14,15 +14,48 @@ describe("anchor-executor", () => {
   it("Is initialized!", async () => {
 
     const newAccount = new anchor.web3.Keypair();
+    const ownerAccount = new anchor.web3.Keypair();
+    
+    const rentExemptionAmount = await provider.connection.getMinimumBalanceForRentExemption(0);
+
+    const createAccountParams = {
+      fromPubkey: provider.wallet.publicKey,
+      newAccountPubkey: newAccount.publicKey,
+      lamports: rentExemptionAmount,
+      space: 0,
+      programId: anchor.web3.SystemProgram.programId
+    };
+ 
+    const createAccountTransaction = new anchor.web3.Transaction().add(
+      anchor.web3.SystemProgram.createAccount(createAccountParams),
+    );
+
+    // console.log(createAccountTransaction.instructions[0])
+
+    let newAccountInfo = await provider.connection.getAccountInfo(newAccount.publicKey);
+    expect(newAccountInfo).to.be.a('null');
 
     // Add your test here.
-    const tx = await program.methods.execute()
-      .remainingAccounts([{
-        pubkey: newAccount.publicKey,
-        isSigner: false,
-        isWritable: false
-      }])
-      .rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.execute(createAccountTransaction.instructions[0])
+      .remainingAccounts([
+        {
+          pubkey: provider.wallet.publicKey,
+          isSigner: true,
+          isWritable: true
+        },
+        {
+          pubkey: newAccount.publicKey,
+          isSigner: true,
+          isWritable: true
+        }
+      ])
+      .signers([newAccount])
+      .rpc()
+      .catch(e => console.error(e));
+
+      newAccountInfo = await provider.connection.getAccountInfo(newAccount.publicKey);
+      expect(newAccountInfo.owner).to.deep.equal(anchor.web3.SystemProgram.programId)
+  
+    // console.log("Your transaction signature", tx);
   });
 });
